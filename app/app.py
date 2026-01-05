fcf9b89c860088f0cb7352a64b7eee51768fd10f
mochthebest-byte <mochthebest-byte@users.noreply.github.com>
2026-01-05 21:41:15 +0200

    Run HTTP server on 8081

--- app/app.py
+++ app/app.py
@@ -6,9 +6,7 @@ PORT = 8081
 class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
-        self.send_header("Content-Type", "text/plain; charset=utf-8")
         self.end_headers()
         self.wfile.write(b"OK\n")
 
-if __name__ == "__main__":
-    HTTPServer((HOST, PORT), Handler).serve_forever()
+HTTPServer((HOST, PORT), Handler).serve_forever()
